a32ec30b4e46ac9fa7bc4b911d202e5ecba77f4a
Keegan Valerio <Keegan.Valerio@gmail.com>
2021-03-18 18:18:56 -0600

    Update loaddata.py

--- loaddata.py
+++ loaddata.py
@@ -2,7 +2,6 @@ import numpy as np
 
 path = 'msl-images/msl_synset_words-indexed.txt'
 
-dat = np.loadtxt(path)
-
-
+dat = np.genfromtxt(path, dtype=str, delimiter="  ", usecols=(0, -1))
+dat = np.char.strip(dat)
 
