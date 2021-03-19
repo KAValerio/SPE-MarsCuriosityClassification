38719825ae071e203f8aacc06d2e547444f19483
unknown <andyb6@gmail.com>
2021-03-18 19:28:50 -0600

    something

--- img_class.py
+++ img_class.py
@@ -1,12 +1,14 @@
 import glob
+import random
 import pandas as pd
 import matplotlib.pyplot as plt
 import matplotlib.image as mpimg
-#from PIL import Image
+import numpy as np
 
 
+# Loading Data
 txt_path = r"C:/Users/andyb/GDrive/Education/DataScienceMentorship/Dataset/Additional_Info"
-img_path = r"C:/Users/andyb/GDrive/Education/DataScienceMentorship/Dataset/Images" #Image.open(img[0])
+img_path = r"C:/Users/andyb/GDrive/Education/DataScienceMentorship/Dataset/Images" 
 
 img = [file_name for file_name in glob.glob(f'{img_path}/*.jpg')]
 txt = [file_name for file_name in glob.glob(f'{txt_path}/*.txt')]
@@ -17,15 +19,24 @@ def load_txt(file_name, cols=["file_name","index_label"]):
         txt_file["file_name"]= txt_file["file_name"].replace(to_replace='calibrated/', value='', regex=True)
     return txt_file
 
-label_map = load_txt(txt[0], ["index_label","label"])
+label_map = load_txt(txt[0], ["index_label","label"]).set_index("index_label").T.to_dict("list")
 test_labels = load_txt(txt[2])
 train_labels = load_txt(txt[3])
 validation_labels = load_txt(txt[4])
 
-plt.figure(figsize=(10, 10))
-for images, labels in test_labels.iteritems():
-  for i in range(9):
-    ax = plt.subplot(3, 3, i + 1)
-    plt.imshow(images[i].numpy().astype("uint8"))
-    plt.title(class_names[labels[i]])
-    plt.axis("off")
+# Exploring Data
+test_labels.describe()
+train_labels.describe()
+validation_labels.describe()
+
+fig,ax = plt.subplots(3,3,figsize=(10,10))
+plt.suptitle('Test Data')
+
+for i in range(3):
+  for j in range(3):
+    x=random.randint(1,test_labels.shape[0])
+    ax[i,j].imshow(mpimg.imread(f'{img_path}/{test_labels.iloc[x][0]}'))
+    ax[i,j].set_title(label_map[test_labels.iloc[x][1]])
+    ax[i,j].axis('off')
+
+plt.show()
